a7bdc5770dbe69da8985021f87b31702619684a7
coolkechang <coolke007@gmail.com>
2023-05-17 12:01:41 +0800

    1.1版本:加备注

--- src/app1.js
+++ src/app1.js
@@ -7,10 +7,13 @@ const $button3 = $("#mul2");
 const $button4 = $("#divide2");
 const $number = $("#number");
 const n = localStorage.getItem("n");
+//只读的localStorage 属性允许访问一个Document 源（origin）的对象 Storage；存储的数据将保存在浏览器会话中。存储在 localStorage 的数据可以长期保留
+//getItem() 作为 Storage 接口的方法，接受一个键名（key name）作为参数，并返回对应键名的值（key's value）。
 $number.text(n || 100);
 
 $button1.on("click", () => {
   let n = parseInt($number.text());
+  //parseInt(string, radix) 解析一个字符串并返回指定基数的十进制整数，radix 是 2-36 之间的整数，表示被解析字符串的基数。
   n += 1;
   localStorage.setItem("n", n);
   $number.text(n);
--- src/app2.js
+++ src/app2.js
@@ -6,14 +6,18 @@ const $tabContent = $("#app2 .tab-content");
 
 $tabBar.on("click", "li", e => {
   const $li = $(e.currentTarget);
+  //Event 接口的只读属性 currentTarget 表示的，标识是当事件沿着 DOM 触发时事件的当前目标。它总是指向事件绑定的元素，而 Event.target 则是事件触发的元素。
   $li
     .addClass("selected")
     .siblings()
     .removeClass("selected");
+  //JS引用CSS时不要用与直接操作CSS相关的API。例如 .css() / .show() / .hide() 等。改用 。addClass() / .removeClass() 等。
+  //样式与控制分离。JS只负责控制，CSS只负责样式。
   const index = $li.index();
   $tabContent
     .children()
     .eq(index)
+    //eq() 方法将匹配元素集缩减值指定 index 上的一个。 .eq(0)指遍历元素并指定第一个。
     .addClass("active")
     .siblings()
     .removeClass("active");
